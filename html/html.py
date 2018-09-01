add8474082e21f0541f488d32898f717fca7334b
Ahmed Jazzar <me@ahmedjazzar.com>
2018-09-01 14:08:19 -0700

    Read from and write to XBlock

--- html/html.py
+++ html/html.py
@@ -17,22 +17,22 @@ class HTML5XBlock(XBlock):
     This XBlock will provide an HTML WYSIWYG interface in Studio to be rendered in LMS.
     """
     display_name = String(
-        display_name=_("Display Name"),
-        help=_("The display name for this component."),
+        display_name=_('Display Name'),
+        help=_('The display name for this component.'),
         scope=Scope.settings,
-        default=_("Text")
+        default=_('Text')
     )
-    data = String(help=_("Html contents to display for this module"), default=u"", scope=Scope.content)
+    data = String(help=_('Html contents to display for this module'), default=u'', scope=Scope.content)
     editor = String(
         help=_(
-            "Select Visual to enter content and have the editor automatically create the HTML. Select Raw to edit "
-            "HTML directly. If you change this setting, you must save the component and then re-open it for editing."
+            'Select Visual to enter content and have the editor automatically create the HTML. Select Raw to edit '
+            'HTML directly. If you change this setting, you must save the component and then re-open it for editing.'
         ),
-        display_name=_("Editor"),
-        default="visual",
+        display_name=_('Editor'),
+        default='visual',
         values=[
-            {"display_name": _("Visual"), "value": "visual"},
-            {"display_name": _("Raw"), "value": "raw"}
+            {'display_name': _('Visual'), 'value': 'visual'},
+            {'display_name': _('Raw'), 'value': 'raw'}
         ],
         scope=Scope.settings
     )
@@ -41,37 +41,45 @@ class HTML5XBlock(XBlock):
     def resource_string(path):
         """Handy helper for getting resources from our kit."""
         data = pkg_resources.resource_string(__name__, path)
-        return data.decode("utf8")
+        return data.decode('utf8')
 
-    @XBlock.supports("multi_device")
+    @XBlock.supports('multi_device')
     def student_view(self, context=None):  # pylint: disable=unused-argument
         """
-        The primary view of the HTML5XBlock, shown to students
-        when viewing courses.
+        Return a fragment that contains the html for the student view
         """
-        html = self.resource_string("static/html/studio.html")
+        html = self.resource_string('static/html/lms.html')
         frag = Fragment(html.format(self=self))
-        frag.add_css(self.resource_string("static/css/html.css"))
-        frag.add_javascript(self.resource_string("static/js/src/html.js"))
-        frag.initialize_js('HTML5XBlock')
+        frag.initialize_js('HTMLXBlock')
         return frag
+
+        return Fragment(self.data)
 
     def studio_view(self, context=None):  # pylint: disable=unused-argument
         html = self.resource_string('static/html/studio.html')
-        frag = Fragment(html)
+        frag = Fragment(html.format(self=self))
         self.add_scripts(frag)
 
         frag.initialize_js('HTML5XBlock')
         return frag
 
+    @XBlock.json_handler
+    def update_content(self, data, suffix=''):  # pylint: disable=unused-argument
+        """
+        This method will update the field, data content with the submitted data from studio_view
+        """
+        self.data = data['content']
+
+        return {'content': self.data}
+
     @staticmethod
     def workbench_scenarios():
         """A canned scenario for display in the workbench."""
         return [
-            ("HTML5XBlock",
+            ('HTML5XBlock',
              """<html5/>
              """),
-            ("Multiple HTML5XBlock",
+            ('Multiple HTML5XBlock',
              """<vertical_demo>
                 <html5/>
                 <html5/>
@@ -85,6 +93,6 @@ class HTML5XBlock(XBlock):
         A helper method to add all necessary scripts to the fragment.
         :param frag: The fragment that will hold the scripts.
         """
-        frag.add_javascript(self.resource_string("static/js/tinymce/tinymce.min.js"))
-        frag.add_javascript(self.resource_string("static/js/tinymce/themes/modern/theme.min.js"))
-        frag.add_javascript(self.resource_string("static/js/entrypoint.js"))
+        frag.add_javascript(self.resource_string('static/js/tinymce/tinymce.min.js'))
+        frag.add_javascript(self.resource_string('static/js/tinymce/themes/modern/theme.min.js'))
+        frag.add_javascript(self.resource_string('static/js/entrypoint.js'))
--- html/static/js/entrypoint.js
+++ html/static/js/entrypoint.js
@@ -1,15 +1,32 @@
-tinymce.init({
-  skin_url: "/resource/html5/public/skin",
-  selector: "textarea",
-  menubar:false,
-  statusbar: false,
-  toolbar: "formatselect | fontselect | bold italic underline forecolor codesample | bullist numlist outdent indent blockquote | link unlink image | code",
-  external_plugins: {
-    'code': '/resource/html5/public/plugins/code/plugin.min.js',
-    'codesample': '/resource/html5/public/plugins/codesample/plugin.min.js',
-    'image': '/resource/html5/public/plugins/image/plugin.min.js',
-    'link': '/resource/html5/public/plugins/link/plugin.min.js',
-    'lists': '/resource/html5/public/plugins/lists/plugin.min.js',
-    'textcolor': '/resource/html5/public/plugins/textcolor/plugin.min.js'
-  }
-});
+/* Javascript for HTMLXBlock. */
+function HTML5XBlock(runtime, element) {
+  $(function ($) {
+    var contentSelector = "textarea#html5-textarea";
+    var pluginsDir = "/resource/html5/public/plugins/";
+    var plugins = ["code", "codesample", "image", "link", "lists", "textcolor"];
+    var externalPlugins = {};
+
+    for (let plugin of plugins) {
+      externalPlugins[plugin] = pluginsDir + plugin + "/plugin.min.js"
+    }
+
+    tinymce.init({
+      skin_url: "/resource/html5/public/skin",
+      selector: contentSelector,
+      menubar: false,
+      statusbar: false,
+      toolbar: "formatselect | fontselect | bold italic underline forecolor codesample | bullist numlist outdent indent blockquote | link unlink image | code",
+      external_plugins: externalPlugins
+    });
+  });
+
+  var handlerUrl = runtime.handlerUrl(element, "update_content");
+  $(".save-button", element).click(function (eventObject) {
+    tinyMCE.triggerSave();
+    $.ajax({
+      type: "POST",
+      url: handlerUrl,
+      data: JSON.stringify({"content": tinymce.get("html5-textarea").getContent()})
+    });
+  });
+}
