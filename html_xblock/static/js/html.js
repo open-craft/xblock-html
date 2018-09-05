be30a9e3e7fb1f85d2abef66e1a3bf0f0df84e7a
Ahmed Jazzar <me@ahmedjazzar.com>
2018-09-05 00:18:34 -0700

    Addressing comments

--- html_xblock/static/js/html.js
+++ html_xblock/static/js/html.js
@@ -1,40 +1,29 @@
 /* Javascript for HTMLXBlock. */
-var CUSTOM_FONTS, STANDARD_FONTS, _getFonts;
 
-CUSTOM_FONTS = "Default='Open Sans', Verdana, Arial, Helvetica, sans-serif;";
-STANDARD_FONTS = "Andale Mono=andale mono,times;" + "Arial=arial,helvetica,sans-serif;" + "Arial Black=arial black,avant garde;" + "Book Antiqua=book antiqua,palatino;" + "Comic Sans MS=comic sans ms,sans-serif;" + "Courier New=courier new,courier;" + "Georgia=georgia,palatino;" + "Helvetica=helvetica;" + "Impact=impact,chicago;" + "Symbol=symbol;" + "Tahoma=tahoma,arial,helvetica,sans-serif;" + "Terminal=terminal,monaco;" + "Times New Roman=times new roman,times;" + "Trebuchet MS=trebuchet ms,geneva;" + "Verdana=verdana,geneva;" + "Webdings=webdings;" + "Wingdings=wingdings,zapf dingbats";
+const CUSTOM_FONTS = "Default='Open Sans', Verdana, Arial, Helvetica, sans-serif;";
+const STANDARD_FONTS = "Andale Mono=andale mono,times;" + "Arial=arial,helvetica,sans-serif;" + "Arial Black=arial black,avant garde;" + "Book Antiqua=book antiqua,palatino;" + "Comic Sans MS=comic sans ms,sans-serif;" + "Courier New=courier new,courier;" + "Georgia=georgia,palatino;" + "Helvetica=helvetica;" + "Impact=impact,chicago;" + "Symbol=symbol;" + "Tahoma=tahoma,arial,helvetica,sans-serif;" + "Terminal=terminal,monaco;" + "Times New Roman=times new roman,times;" + "Trebuchet MS=trebuchet ms,geneva;" + "Verdana=verdana,geneva;" + "Webdings=webdings;" + "Wingdings=wingdings,zapf dingbats";
+const FONTS = CUSTOM_FONTS + STANDARD_FONTS;
 
-_getFonts = function () {
-  return CUSTOM_FONTS + STANDARD_FONTS;
-};
-
-var PLUGINS, PLUGINS_DIR, _getExternalPlugins;
-
-PLUGINS = ["codesample", "image", "link", "lists", "textcolor", "codemirror"];
-PLUGINS_DIR = "/resource/html5/public/plugins/";
-
-_getExternalPlugins = function () {
-  var externalPlugins = {};
-
-  for (let plugin of PLUGINS) {
-    externalPlugins[plugin] = PLUGINS_DIR + plugin + "/plugin.min.js"
-  }
-  return externalPlugins
-};
+const PLUGINS = ["codesample", "image", "link", "lists", "textcolor", "codemirror"];
+const PLUGINS_DIR = "/resource/html5/public/plugins/";
+const EXTERNAL_PLUGINS = PLUGINS.map(function(p) { return PLUGINS_DIR + p + "/plugin.min.js" });
 
 function HTML5XBlock(runtime, element) {
-  var editorChoice = $("#editor-tab").data("editor");
-  var contentSelector = "textarea#html5-textarea";
+  const editorTab = document.getElementById("editor-tab");
+  const contentSelector = "textarea#html5-textarea";
   var editor;
 
-  $(function ($) {
-    if (editorChoice === "visual") {
+  window.addEventListener('load', function () {
+    const languageWrapper = document.querySelectorAll(".wrapper-view, .window-wrap");
+    const directionality = (languageWrapper.length > 0) ? languageWrapper.dir : "ltr";
+
+    if (editorTab.dataset.editor === "visual") {
       editor = tinymce.init({
         skin_url: "/resource/html5/public/skin",
         theme: "modern",
         schema: "html5",
         convert_urls: false,
-        directionality: $(".wrapper-view, .window-wrap").prop('dir'),
+        directionality: directionality,
         selector: contentSelector,
         menubar: false,
         statusbar: false,
@@ -42,9 +31,9 @@ function HTML5XBlock(runtime, element) {
         extended_valid_elements: "*[*]",
         valid_children: "+body[style]",
         invalid_elements: "",
-        font_formats: _getFonts(),
+        font_formats: FONTS,
         toolbar: "formatselect | fontselect | bold italic underline forecolor codesample | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent blockquote | link unlink image | code",
-        external_plugins: _getExternalPlugins(),
+        external_plugins: EXTERNAL_PLUGINS,
         formats: {
           code: {
             inline: 'code'
@@ -67,18 +56,18 @@ function HTML5XBlock(runtime, element) {
         }
       });
     } else {
-      editor = CodeMirror.fromTextArea($(contentSelector, this.element)[0], {
+      editor = CodeMirror.fromTextArea(document.querySelectorAll(contentSelector)[0], {
         mode: "text/html",
         lineNumbers: true,
         matchBrackets: true,
         lineWrapping: true
       });
     }
-  });
+  }, false);
 
-  $(".save-button", element).click(function (eventObject) {
-    var handlerUrl = runtime.handlerUrl(element, "update_content");
-    var content = (editorChoice === "visual") ? tinymce.get("html5-textarea").getContent() : editor.getValue();
+  document.getElementById("save-button").addEventListener("click", function () {
+    const handlerUrl = runtime.handlerUrl(element, "update_content");
+    const content = (editorTab.dataset.editor === "visual") ? tinymce.get("html5-textarea").getContent() : editor.getValue();
 
     $.ajax({
       type: "POST",
