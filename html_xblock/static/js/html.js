43457e07e443d0a7d4c3382c2f797efe96e5e98a
Ahmed Jazzar <me@ahmedjazzar.com>
2018-09-17 17:11:52 -0700

    Optimize JS

--- html_xblock/static/js/html.js
+++ html_xblock/static/js/html.js
@@ -9,17 +9,17 @@ function openTab(evt, tabName) {
    * This logic has been taken from https://www.w3schools.com/howto/howto_js_tabs.asp
    */
 
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
 }
 
 function HTML5XBlock(runtime, element, data) {
@@ -78,192 +78,170 @@ function HTML5XBlock(runtime, element, data) {
     });
   }
 
-  document.getElementById("html5-save-button").addEventListener("click", function () {
-    const handlerUrl = runtime.handlerUrl(element, "update_content");
-    const content = (data.editor === "visual") ? tinymce.get("html5-textarea").getContent() : editor.getValue();
+  var fields = [];
 
-    if (runtime.notify) {
-      runtime.notify('save', {state: 'start'});
-    }
-    $.post(handlerUrl, JSON.stringify({"content": content})).done(function (response) {
-      if (runtime.notify) {
-        runtime.notify('save', {state: 'end'});
+  var elements = document.querySelectorAll(".field-data-control");
+  Array.prototype.forEach.call(elements, function (item) {
+    var $field = $(item);
+    var $wrapper = $field.closest('li');
+    var $resetButton = $wrapper.find('button.setting-clear');
+    var type = $wrapper.data('cast');
+    var datepickerAvailable = (typeof $.fn.datepicker !== 'undefined'); // Studio includes datepicker jQuery plugin
+
+    fields.push({
+      name: $wrapper.data('field-name'),
+      isSet: function () {
+        return $wrapper.hasClass('is-set');
+      },
+      val: function () {
+        var val = $field.val();
+        // Cast values to the appropriate type so that we send nice clean JSON over the wire:
+        if (type == 'boolean')
+          return (val == 'true' || val == '1');
+        if (type == "integer")
+          return parseInt(val, 10);
+        if (type == "float")
+          return parseFloat(val);
+        if (type == "generic" || type == "list" || type == "set") {
+          val = val.trim();
+          if (val === "")
+            val = null;
+          else
+            val = JSON.parse(val);
+        }
+        return val;
+      },
+      removeEditor: function () {
+        $field.tinymce().remove();
       }
+    });
+    var fieldChanged = function () {
+      // Field value has been modified:
+      $wrapper.addClass('is-set');
+      $resetButton.removeClass('inactive').addClass('active');
+    };
+    $field.bind("change input paste", fieldChanged);
+    $resetButton.click(function () {
+      $field.val($wrapper.attr('data-default')); // Use attr instead of data to force treating the default value as a string
+      $wrapper.removeClass('is-set');
+      $resetButton.removeClass('active').addClass('inactive');
+    });
+
+    if (type == 'datepicker' && datepickerAvailable) {
+      $field.datepicker('destroy');
+      $field.datepicker({dateFormat: "m/d/yy"});
+    }
+  });
+
+  elements = document.querySelectorAll(".wrapper-list-settings .list-set");
+  Array.prototype.forEach.call(elements, function (item) {
+    var $optionList = $(item);
+    var $checkboxes = $(item).find('input');
+    var $wrapper = $optionList.closest('li');
+    var $resetButton = $wrapper.find('button.setting-clear');
+
+    fields.push({
+      name: $wrapper.data('field-name'),
+      isSet: function () {
+        return $wrapper.hasClass('is-set');
+      },
+      hasEditor: function () {
+        return false;
+      },
+      val: function () {
+        var val = [];
+        $checkboxes.each(function () {
+          if ($(item).is(':checked')) {
+            val.push(JSON.parse($(item).val()));
+          }
+        });
+        return val;
+      }
+    });
+    var fieldChanged = function () {
+      // Field value has been modified:
+      $wrapper.addClass('is-set');
+      $resetButton.removeClass('inactive').addClass('active');
+    };
+    $checkboxes.bind("change input", fieldChanged);
+
+    $resetButton.click(function () {
+      var defaults = JSON.parse($wrapper.attr('data-default'));
+      $checkboxes.each(function () {
+        var val = JSON.parse($(item).val());
+        $(item).prop('checked', defaults.indexOf(val) > -1);
+      });
+      $wrapper.removeClass('is-set');
+      $resetButton.removeClass('active').addClass('inactive');
     });
   });
 
-    var fields = [];
-    var tinyMceAvailable = (typeof $.fn.tinymce !== 'undefined'); // Studio includes a copy of tinyMCE and its jQuery plugin
-    var datepickerAvailable = (typeof $.fn.datepicker !== 'undefined'); // Studio includes datepicker jQuery plugin
+  var studio_submit = function (fields_data) {
+    const ContentHandlerUrl = runtime.handlerUrl(element, "update_content");
+    const SettingsHandlerUrl = runtime.handlerUrl(element, "submit_studio_edits");
+    const content = (data.editor === "visual") ? tinymce.get("html5-textarea").getContent() : editor.getValue();
+    var errorMessage = "This may be happening because of an error with our server or your internet connection. Try refreshing the page or making sure you are online.";
 
-    $(element).find('.field-data-control').each(function() {
-        var $field = $(this);
-        var $wrapper = $field.closest('li');
-        var $resetButton = $wrapper.find('button.setting-clear');
-        var type = $wrapper.data('cast');
-        fields.push({
-            name: $wrapper.data('field-name'),
-            isSet: function() { return $wrapper.hasClass('is-set'); },
-            hasEditor: function() { return tinyMceAvailable && $field.tinymce(); },
-            val: function() {
-                var val = $field.val();
-                // Cast values to the appropriate type so that we send nice clean JSON over the wire:
-                if (type == 'boolean')
-                    return (val == 'true' || val == '1');
-                if (type == "integer")
-                    return parseInt(val, 10);
-                if (type == "float")
-                    return parseFloat(val);
-                if (type == "generic" || type == "list" || type == "set") {
-                    val = val.trim();
-                    if (val === "")
-                        val = null;
-                    else
-                        val = JSON.parse(val); // TODO: handle parse errors
-                }
-                return val;
-            },
-            removeEditor: function() {
-                $field.tinymce().remove();
-            }
-        });
-        var fieldChanged = function() {
-            // Field value has been modified:
-            $wrapper.addClass('is-set');
-            $resetButton.removeClass('inactive').addClass('active');
-        };
-        $field.bind("change input paste", fieldChanged);
-        $resetButton.click(function() {
-            $field.val($wrapper.attr('data-default')); // Use attr instead of data to force treating the default value as a string
-            $wrapper.removeClass('is-set');
-            $resetButton.removeClass('active').addClass('inactive');
-        });
-        if (type == 'html' && tinyMceAvailable) {
-            tinyMCE.baseURL = baseUrl + "/js/vendor/tinymce/js/tinymce";
-            $field.tinymce({
-                theme: 'modern',
-                skin: 'studio-tmce4',
-                height: '200px',
-                formats: { code: { inline: 'code' } },
-                codemirror: { path: "" + baseUrl + "/js/vendor" },
-                convert_urls: false,
-                plugins: "link codemirror",
-                menubar: false,
-                statusbar: false,
-                toolbar_items_size: 'small',
-                toolbar: "formatselect | styleselect | bold italic underline forecolor wrapAsCode | bullist numlist outdent indent blockquote | link unlink | code",
-                resize: "both",
-                setup : function(ed) {
-                    ed.on('change', fieldChanged);
-                }
-            });
-        }
-
-        if (type == 'datepicker' && datepickerAvailable) {
-            $field.datepicker('destroy');
-            $field.datepicker({dateFormat: "m/d/yy"});
-        }
-    });
-
-    $(element).find('.wrapper-list-settings .list-set').each(function() {
-        var $optionList = $(this);
-        var $checkboxes = $(this).find('input');
-        var $wrapper = $optionList.closest('li');
-        var $resetButton = $wrapper.find('button.setting-clear');
-
-        fields.push({
-            name: $wrapper.data('field-name'),
-            isSet: function() { return $wrapper.hasClass('is-set'); },
-            hasEditor: function() { return false; },
-            val: function() {
-                var val = [];
-                $checkboxes.each(function() {
-                    if ($(this).is(':checked')) {
-                        val.push(JSON.parse($(this).val()));
-                    }
-                });
-                return val;
-            }
-        });
-        var fieldChanged = function() {
-            // Field value has been modified:
-            $wrapper.addClass('is-set');
-            $resetButton.removeClass('inactive').addClass('active');
-        };
-        $checkboxes.bind("change input", fieldChanged);
-
-        $resetButton.click(function() {
-            var defaults = JSON.parse($wrapper.attr('data-default'));
-            $checkboxes.each(function() {
-                var val = JSON.parse($(this).val());
-                $(this).prop('checked', defaults.indexOf(val) > -1);
-            });
-            $wrapper.removeClass('is-set');
-            $resetButton.removeClass('active').addClass('inactive');
-        });
-    });
-
-    var studio_submit = function(data) {
-        var handlerUrl = runtime.handlerUrl(element, 'submit_studio_edits');
-        runtime.notify('save', {state: 'start', message: gettext("Saving")});
-        $.ajax({
+    runtime.notify('save', {state: 'start', message: "Saving"});
+    $.ajax({
+        type: "POST",
+        url: SettingsHandlerUrl,
+        data: JSON.stringify(fields_data),
+        dataType: "json",
+        global: false,  // Disable Studio's error handling that conflicts with studio's notify('save') and notify('cancel') :-/
+        success: function(response) {
+          $.ajax({
             type: "POST",
-            url: handlerUrl,
-            data: JSON.stringify(data),
+            url: ContentHandlerUrl,
+            data: JSON.stringify({"content": content}),
             dataType: "json",
             global: false,  // Disable Studio's error handling that conflicts with studio's notify('save') and notify('cancel') :-/
             success: function(response) { runtime.notify('save', {state: 'end'}); }
-        }).fail(function(jqXHR) {
-            var message = gettext("This may be happening because of an error with our server or your internet connection. Try refreshing the page or making sure you are online.");
-            if (jqXHR.responseText) { // Is there a more specific error message we can show?
-                try {
-                    message = JSON.parse(jqXHR.responseText).error;
-                    if (typeof message === "object" && message.messages) {
-                        // e.g. {"error": {"messages": [{"text": "Unknown user 'bob'!", "type": "error"}, ...]}} etc.
-                        message = $.map(message.messages, function(msg) { return msg.text; }).join(", ");
-                    }
-                } catch (error) { message = jqXHR.responseText.substr(0, 300); }
-            }
-            runtime.notify('error', {title: gettext("Unable to update settings"), message: message});
-        });
-    };
-
-    $('.save-button', element).bind('click', function(e) {
-        e.preventDefault();
-        var values = {};
-        var notSet = []; // List of field names that should be set to default values
-        for (var i in fields) {
-            var field = fields[i];
-            if (field.isSet()) {
-                values[field.name] = field.val();
-            } else {
-                notSet.push(field.name);
-            }
-            // Remove TinyMCE instances to make sure jQuery does not try to access stale instances
-            // when loading editor for another block:
-            if (field.hasEditor()) {
-                field.removeEditor();
-            }
+          }).fail(function (jqXHR) {
+            runtime.notify('error', {title: "Unable to update content", message: errorMessage});
+          })
         }
-        studio_submit({values: values, defaults: notSet});
-    });
-
-    $(element).find('.cancel-button').bind('click', function(e) {
-        // Remove TinyMCE instances to make sure jQuery does not try to access stale instances
-        // when loading editor for another block:
-        for (var i in fields) {
-            var field = fields[i];
-            if (field.hasEditor()) {
-                field.removeEditor();
-            }
+    }).fail(function (jqXHR) {
+      if (jqXHR.responseText) { // Is there a more specific error message we can show?
+        try {
+          errorMessage = JSON.parse(jqXHR.responseText).error;
+          if (typeof errorMessage === "object" && errorMessage.messages) {
+            // e.g. {"error": {"messages": [{"text": "Unknown user 'bob'!", "type": "error"}, ...]}} etc.
+            errorMessage = $.map(errorMessage.messages, function (msg) {
+              return msg.text;
+            }).join(", ");
+          }
+        } catch (error) {
+          errorMessage = jqXHR.responseText.substr(0, 300);
         }
-        e.preventDefault();
-        runtime.notify('cancel', {});
-    });
+      }
+      runtime.notify('error', {title: "Unable to update settings", message: errorMessage});
+    })
+  };
 
-  document.getElementById("html5-cancel-button").addEventListener("click", function () {
-    if (runtime.notify) {
+  elements = document.querySelectorAll(".save-button");
+  Array.prototype.forEach.call(elements, function (elem) {
+    elem.addEventListener("click", function (e) {
+      e.preventDefault();
+      var values = {};
+      var notSet = []; // List of field names that should be set to default values
+      for (var i in fields) {
+        var field = fields[i];
+        if (field.isSet()) {
+          values[field.name] = field.val();
+        } else {
+          notSet.push(field.name);
+        }
+      }
+      studio_submit({values: values, defaults: notSet});
+    });
+  });
+
+  elements = document.querySelectorAll(".cancel-button");
+  Array.prototype.forEach.call(elements, function (elem) {
+    elem.addEventListener("click", function (e) {
+      e.preventDefault();
       runtime.notify('cancel', {});
-    }
+    });
   });
 }
