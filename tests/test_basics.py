d6ce3460577bb5e432ec48afe507eb474f77ac17
Gustavo Sampaio <gbritosampaio@gmail.com>
2018-11-26 15:16:02 -0300

    Add tests docstrings

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -19,6 +19,9 @@ class TestHTMLXBlock(unittest.TestCase):
         self.runtime = TestRuntime()
 
     def test_render(self):
+        """
+        Test a basic rendering with default settings.
+        """
         field_data = DictFieldData({'data': 'Safe <b>html</b>'})
         block = html_xblock.HTML5XBlock(self.runtime, field_data, None)
         self.assertEqual(block.allow_javascript, False)
@@ -26,6 +29,10 @@ class TestHTMLXBlock(unittest.TestCase):
         self.assertIn('<div>Safe <b>html</b></div>', fragment.content)
 
     def test_render_with_unsafe(self):
+        """
+        Test a basic rendering with default settings.
+        Expects the content to be sanitized.
+        """
         field_data = DictFieldData({'data': 'Safe <b>html</b><script>alert(\'javascript\');</script>'})
         block = html_xblock.HTML5XBlock(self.runtime, field_data, None)
         self.assertEqual(block.allow_javascript, False)
@@ -36,6 +43,10 @@ class TestHTMLXBlock(unittest.TestCase):
         )
 
     def test_render_allow_js(self):
+        """
+        Test a basic rendering with javascript enabled.
+        Expects the content *not* to be sanitized.
+        """
         field_data = DictFieldData({
             'data': 'Safe <b>html</b><script>alert(\'javascript\');</script>',
             'allow_javascript': True
